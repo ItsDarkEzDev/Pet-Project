4a06a0a9f663d35db707c384abe38609536fa11b
ItsDarkEzDev <76558172+ItsDarkEzDev@users.noreply.github.com>
2021-08-03 18:17:34 +0500

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $date = [
     'port' => 3306
     ];
 
-$mysqli = new mysqli($date['host'], $date['login'], $date['password'], $date['database']);
+$mysqli = @new mysqli($date['host'], $date['login'], $date['password'], $date['database']);
 if($mysqli->connect_errno) return exit('MySQL error!');
 $mysqli->set_charset("utf8");
 
